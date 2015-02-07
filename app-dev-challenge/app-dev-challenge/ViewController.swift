6e64e47c38b70090c62183e21643d7dd5776f0aa
hollyhastings <hhastings111@gmail.com>
2015-02-06 20:36:31 -0500

    Get element sprites to appear on screen

--- app-dev-challenge/app-dev-challenge/ViewController.swift
+++ app-dev-challenge/app-dev-challenge/ViewController.swift
@@ -3,6 +3,7 @@
 //  app-dev-challenge
 //
 //  Created by Morgan Cabral on 2/5/15.
+//  Code by Holly Hastings
 //  Copyright (c) 2015 Team SwiftRekt. All rights reserved.
 //
 
@@ -13,13 +14,14 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    let scene = SKScene(size: view.bounds.size)
-    let spriteView = view as SKView
+    
+    // Scene stuff
+    let scene = LoadingScene(size: view.bounds.size)
+    let spriteView : SKView = self.view as SKView;
     spriteView.showsDrawCount = true;  spriteView.showsNodeCount = true; spriteView.showsFPS = true; spriteView.ignoresSiblingOrder = true
     scene.scaleMode = .ResizeFill
     spriteView.presentScene(scene)
-    
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
 
   override func didReceiveMemoryWarning() {
@@ -28,5 +30,35 @@ class ViewController: UIViewController {
   }
 
 
+}
+
+class LoadingScene: SKScene {
+    
+    // Create element sprite nodes
+    let fire = SKSpriteNode(imageNamed: "RedFire")
+    let water = SKSpriteNode(imageNamed: "BlueWater")
+    let earth = SKSpriteNode(imageNamed: "YellowEarth")
+    let air = SKSpriteNode(imageNamed: "GreenAir")
+    
+    override func didMoveToView(view: SKView) {
+        backgroundColor = SKColor.darkGrayColor()
+        
+        // Positions of element sprites
+        //      F
+        //  E   +   A
+        //      W
+        //
+        fire.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
+        water.position = CGPoint(x: size.width * 0.5, y: size.height * 0.3)
+        earth.position = CGPoint(x: size.width * 0.2, y: size.height * 0.5)
+        air.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        
+        //add children
+        addChild(fire)
+        addChild(water)
+        addChild(earth)
+        addChild(air)
+        
+    }
 }
 
