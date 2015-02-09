0ecc2cb769c9d8ecca0ba67aa381b48af81d81d1
pxk7062 <pxk7062@rit.edu>
2015-02-08 20:26:44 -0500

    added some spacings

--- app-dev-challenge/app-dev-challenge/PlayerPortraitSprite.swift
+++ app-dev-challenge/app-dev-challenge/PlayerPortraitSprite.swift
@@ -5,7 +5,8 @@
 //  Created by Morgan Cabral on 2/7/15.
 //  Copyright (c) 2015 Team SwiftRekt. All rights reserved.
 //
-
+import UIKit
+import AVFoundation
 import Foundation
 
 /*
@@ -14,5 +15,74 @@ import Foundation
  * to cast a spell.
  */
 public class PlayerPortraitSprite {
-  
+
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var capturedImage: UIImageView!
+    @IBOutlet weak var takePhotoButton: UIButton!
+    
+    var captureSession: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+
+    func viewWillAppear(animated: Bool) {
+        
+        captureSession = AVCaptureSession()
+        captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error: NSError?
+        var input = AVCaptureDeviceInput(device: backCamera, error: &error)
+        
+        if error == nil && captureSession!.canAddInput(input) {
+            captureSession!.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput!.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            if captureSession!.canAddOutput(stillImageOutput) {
+                captureSession!.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.Portrait
+                previewView.layer.addSublayer(previewLayer)
+                
+                captureSession!.startRunning()
+            }
+        }
+        
+    }
+    
+    func viewDidAppear(animated: Bool) {
+        previewLayer!.frame = previewView.bounds
+    }
+
+    @IBAction func didPressTakePhoto(sender: AnyObject) {
+        
+        if let videoConnection = stillImageOutput!.connectionWithMediaType(AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
+                if (sampleBuffer != nil) {
+                    var imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    var dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    var cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, kCGRenderingIntentDefault)
+                    
+                    var image = UIImage(CGImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.Right)
+                    self.capturedImage.image = image
+                    
+                    let redCount = 0;
+                    let greenCount = 0;
+                    let blueCount = 0;
+                    
+                    
+                    
+                }
+            })
+        }
+    }
+    
+    @IBAction func didPressTakeAnother(sender: AnyObject) {
+        captureSession!.startRunning()
+    }
 }
