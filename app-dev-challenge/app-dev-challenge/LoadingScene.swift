6ad8032beb7321838b92a0744d22b091886e0bfd
Morgan Cabral <morgan.cabral@gmail.com>
2015-02-11 20:11:46 -0500

    Changed the background color of the loading screen sprite to match the view.

--- app-dev-challenge/app-dev-challenge/LoadingScene.swift
+++ app-dev-challenge/app-dev-challenge/LoadingScene.swift
@@ -14,7 +14,7 @@ import Foundation
 class LoadingScene: SKScene {
 
   override func didMoveToView(view: SKView) {
-    backgroundColor = SKColor.greenColor()
+    backgroundColor = UIColor.grayColor()
     
     // This is radius of the circle path.
     var r = 90.0
